06444604ed26a1aff9323659bfe60369605aa329
kleislicat <79645287+kleislicat@users.noreply.github.com>
2025-07-14 19:55:31 +0100

    Creational

--- src/com/coffeepoweredcrew/singleton/RegistryEnum.java
+++ src/com/coffeepoweredcrew/singleton/RegistryEnum.java
@@ -2,7 +2,7 @@ package com.coffeepoweredcrew.singleton;
 
 /**
  * (Ref. Google I/O 2k8 Joshua Bloch) Since Java 1.5 using enum is clean way to create a singleton.
- * It handles serialization using java's in-built mechanism and still ensure single instance
+ * It handles serialization using java's in-built mechanism and still ensure single instance.
  */
 public enum RegistryEnum {
 
