ee84d496ed5cb669e1ee800465bc148e1f3d4bfd
kleislicat <79645287+kleislicat@users.noreply.github.com>
2025-07-16 14:12:05 +0100

    behavioural pattern

---
+++ src/com/mydomain/behavioural/chainofresponsibility/LeaveApprover.java
@@ -0,0 +1,9 @@
+package com.mydomain.behavioural.chainofresponsibility;
+
+//This represents a handler in chain of responsibility
+public interface LeaveApprover {
+
+  void processLeaveApplication(LeaveApplication application);
+
+  String getApproverRole();
+}
